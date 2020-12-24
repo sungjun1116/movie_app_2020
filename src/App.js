c935d96c40ac6ea31a595a0fb8e81096c856552d
Sungjun <opr99@naver.com>
2020-12-24 22:30:19 +0900

    #4.0 Fetching Movies from API

--- src/App.js
+++ src/App.js
@@ -1,36 +1,24 @@
 import React from 'react';
+import axios from 'axios';
 import PropTypes from 'prop-types';
 
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+  // 미래에 쓰고자 하는 state를 선언하는 것이 필수는 아님
   state = {
-    count: 0
+    isLoading: true,
+    movies: []
   };
-  add = () => {
-    this.setState(current => ({ count: current.count + 1 }));
-  };
-  minus = () => {
-    this.setState(current => ({ count: current.count - 1 }));
-  };
+  // async-awit를 사용하지 않으면 javascript가 axios.get을 기다려주지 않음
+  getMovies = async () => {
+    const movies = await axios.get("https://yts-proxy.now.sh/list_movies.json");
+  }
   componentDidMount() {
-    console.log('component rendered');
-  }
-  componentDidUpdate() {
-    console.log('I just updated');
-  }
-  componentWillUnmount() {
-    console.log('Goodbye, cruel world');
+    this.getMovies();
   }
   render() {
-    console.log("I'm rendering");
-    return <div>
-      <h1>The number is: {this.state.count}</h1>
-      <button onClick={this.add}>Add</button>
-      <button onClick={this.minus}>Minus</button>
-    </div>
+    const { isLoading } = this.state;
+    return <div>{isLoading ? "Loading..." : "We are ready"}</div>;
   }
 }
 
