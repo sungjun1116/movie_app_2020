50a829566ced122792414331cb241a4578ca7953
Sungjun <opr99@naver.com>
2020-12-25 16:07:27 +0900

    #6.4 Redirecting

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from "react";
 import { HashRouter, Route } from "react-router-dom";
 import Home from "./routes/Home";
 import About from "./routes/About";
-import Detail from "./components/Detail";
+import Detail from "./routes/Detail";
 import Navigation from "./components/Navigation";
 import "./App.css";
 
@@ -13,7 +13,7 @@ function App() {
       <Navigation />
       <Route path="/" exact={true} component={Home} />
       <Route path="/about" component={About} />
-      <Route path="/movie-detail" component={Detail} />
+      <Route path="/movie/:id" component={Detail} />
     </HashRouter>
   );
 }
