d85ee42ba74f824ac722885c8bed241e1c133c49
Sungjun <opr99@naver.com>
2020-12-25 14:46:59 +0900

    #6.1 Building the Router

--- src/App.js
+++ src/App.js
@@ -1,56 +1,16 @@
 /** @format */
-
 import React from "react";
-import axios from "axios";
-import Movie from "./Movie";
-import "./App.css";
+import { HashRouter, Route } from "react-router-dom";
+import About from "./routes/About";
+import Home from "./routes/Home";
 
-class App extends React.Component {
-  // 미래에 쓰고자 하는 state를 선언하는 것이 필수는 아님
-  state = {
-    isLoading: true,
-    movies: [],
-  };
-  // async-awit를 사용하지 않으면 javascript가 axios.get을 기다려주지 않음
-  getMovies = async () => {
-    const {
-      data: {
-        data: { movies },
-      },
-    } = await axios.get(
-      "https://yts-proxy.now.sh/list_movies.json?sort_by=rating"
-    );
-    this.setState({ movies, isLoading: false }); // {movies:movies}의 ES6버전
-  };
-  componentDidMount() {
-    this.getMovies();
-  }
-  render() {
-    const { isLoading, movies } = this.state;
-    return (
-      <section className="container">
-        {isLoading ? (
-          <div className="loader">
-            <span className="loader__text">Loading...</span>
-          </div>
-        ) : (
-          <div className="movies">
-            {movies.map((movie) => (
-              <Movie
-                key={movie.id}
-                id={movie.id}
-                year={movie.year}
-                title={movie.title}
-                summary={movie.summary}
-                poster={movie.medium_cover_image}
-                genres={movie.genres}
-              />
-            ))}
-          </div>
-        )}
-      </section>
-    );
-  }
+function App() {
+  return (
+    <HashRouter>
+      <Route path="/" exact={true} component={Home} />
+      <Route path="/about" component={About} />
+    </HashRouter>
+  );
 }
 
 export default App;
---
+++ src/routes/About.js
@@ -0,0 +1,9 @@
+/** @format */
+
+import React from "react";
+
+function About() {
+  return <span>About this page: I built it becuase I love moveis.</span>;
+}
+
+export default About;
